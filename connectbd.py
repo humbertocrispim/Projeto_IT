af5033645e7691b213e61897e42cded5aade669c
humbertocrispim <humberto.cunha.crispim@gmail.com>
2022-06-13 16:15:10 -0300

    ok

--- connectbd.py
+++ connectbd.py
@@ -4,7 +4,7 @@ con = mysql.connector.connect(
     host='localhost',
     user='root',
     password='S@xa2115',
-    database='testconect'
+    database='teste'
 )
 
 mycursor = con.cursor()
@@ -20,7 +20,7 @@ def CriarBase():
 # isert tables
 
 def Criartabela():
-    mycursor.execute("CREATE TABLE clientes (nome TEXT, senha TEXT)")
+    mycursor.execute("CREATE TABLE  (nome TEXT, senha TEXT)")
 
 
 
@@ -33,13 +33,27 @@ def Altertable():
 
 def InserirDados():
     input_nome = input("Digite seu nome: ")
-    input_senha = input("digite sua senha: ")
+    input_endereco = input("digite ser endereço: ")
+    input_idade = input("digite a idade do paciente: ")
+    input_cpf = input("digite o CPF do paciente: ")
+    input_rg = input("igite o RG do paciente: ")
+    input_sexo = input("igite o sexo do paciente: ")
+    input_cep = input("igite o CEP do paciente: ")
+    input_bairro = input("igite o bairro do paciente: ")
+    input_cidade = input("igite o cidade do paciente: ")
+    input_estado = input("igite o estado do paciente: ")
+    input_telefone = input("igite o telefone do paciente: ")
+    input_altura = input("igite o altura do paciente: ")
+    input_peso = input("igite o peso do paciente: ")
 
-    comando_sql = "INSERT INTO clientes (nome, senha) VALUES(%s,%s)"
-    valores = (input_nome, input_senha)
+
+    comando_sql = "INSERT INTO Paciente (nome,endereco,idade,cpf,rg,sexo,cep,bairro,cidade,estado,telefone,altura,peso) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    valores = (input_nome, input_endereco,input_idade,input_cpf,input_rg,input_sexo,input_cep,input_bairro,input_cidade,input_estado,input_telefone,input_altura,input_peso)
 
     mycursor.execute(comando_sql, valores)
     con.commit()
 
 
-Altertable()
+
+InserirDados()
+#   INSERT INTO teste.Paciente (nome,endereco,idade,cpf,rg,sexo,mae,cep,bairro,cidade,estado,telefone_fixo,celular,altura)
